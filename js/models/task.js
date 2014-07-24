3c529558f5b1c91efa137fb6f6c1c1f505db99f3
jongrover <hello@jonathangrover.com>
2014-07-24 01:34:50 -0400

    separate to controller

--- js/models/task.js
+++ js/models/task.js
@@ -16,31 +16,3 @@ Task.prototype = {
     $('#list-'+this.list.id).append(this.el());
   }
 };
-Task.init = function() {
-
-  var $addTaskForm = $('#add_task'),
-      $selectListMenu = $('#select_list'),
-      $taskDescriptionInput = $('#task_description'),
-      $taskPriorityInput = $('#task_priority'),
-      $wrapper = $('#wrapper');
-
-  $addTaskForm.submit(function(event) {
-    event.preventDefault();
-    var listId = parseInt($selectListMenu.val()),
-        taskDescription = $taskDescriptionInput.val(),
-        taskPriority = $taskPriorityInput.val(),
-        task = new Task(taskDescription, taskPriority, List.all[listId]);
-    task.build();
-    $taskDescriptionInput.val('');
-    $taskPriorityInput.val('');
-  });
-
-  $wrapper.on('click', '.destroy-task', function(){ //live event listener
-    var listId = parseInt($(this).parents('ul').data('id')),
-        taskId = parseInt($(this).parent('li').data('id')),
-        list = List.all[listId];
-        list.tasks.splice(taskId, 1, null);
-    $(this).parent('li').remove();
-  });
-
-};
