093591aa4287ef9e389ed22cf6e8b0238afd6826
github username <andrewcohn0@gmail.com>
2017-09-19 10:04:25 -0400

    added basic app level methods & hide new task form when there are no lists

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,25 @@ class App {
   constructor() {
     this.lists = []
     this.initializeEventListeners() //grab DOM elements and attach event listeners
+    this.render()
   }
-  
-  initializeEventListeners() {}
+
+  initializeEventListeners() {
+    //new list form and input field
+    this.createListForm = document.getElementById("create-list-form")
+    this.newListTitle = document.getElementById("new-list-title")
+    //new task form and input fields
+    this.createTaskForm = document.getElementById("create-task-form")
+    this.parentListDropdown = document.getElementById("parent-list") //dropdown select
+    this.newTaskDescription = document.getElementById("new-task-description")
+    this.newTaskPriority = document.getElementById("new-task-priority")
+    this.listsSection = document.getElementById("lists")//section of page where lists will be rendered to
+
+    //attach event listeners
+    this.createListForm.addEventListener("submit", this.createNewList)
+    this.createTaskForm.addEventListener("submit", this.createNewTask)
+    this.listsSection.addEventListener("click", this.deleteList)
+  }
 
   createNewList() {}
 
@@ -12,6 +28,8 @@ class App {
 
   deleteList() {}
 
-  render() {/*TODO check if we have any lists and hide the form accordingly*/}
+  render() {
+    this.lists.length === 0 ? this.createTaskForm.style.display = "none" : this.createTaskForm.style.display = "block"
+  }
 
 }
