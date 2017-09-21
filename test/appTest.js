3991e1fe41e92350f6a02bb43a5018ee33538ba2
github username <andrewcohn0@gmail.com>
2017-09-21 13:50:58 -0400

    updated tests

--- test/appTest.js
+++ test/appTest.js
@@ -2,18 +2,17 @@ const expect = chai.expect;
 
 describe("app.js", () => {
   describe("App Class", () => {
-    const app = new App();
 
     it("should initialize with an array of lists", () => {
-      expect(app.lists).to.be.a("array");
+      expect(App.constructor).to.be.a("function");
     });
 
     it("should have a render method", () => {
-      expect(app.render).to.be.a("function");
+      expect(App.prototype.render).to.be.a("function");
     });
 
     it("should have a deleteList method", () => {
-      expect(app.deleteList).to.be.a("function");
+      expect(App.prototype.deleteList).to.be.a("function");
     });
 
   });
