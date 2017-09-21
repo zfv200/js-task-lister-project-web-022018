2837aa1823c281272503e62c293ccf12b8b82f79
github username <andrewcohn0@gmail.com>
2017-09-21 17:58:37 -0400

    gutted solution and updated readme

--- src/components/task.js
+++ src/components/task.js
@@ -1,23 +1,11 @@
 /*
 task is responsible for creating a single task object
 */
-
 const Task = (() => {
   let id = 1
-
   return class Task {
     constructor(description, priority) {
-      if (!description) { throw new Error("Form description cannot be empty") } //all tasks must have a description
-
-      this.id = id
-      this.description = description
-      this.priority = priority || "low" //if no priority is given, default to low
-      id++
-    }
-    render() {
-      return (
-        `<li data-id="${this.id}">Task: ${this.description} <br> Priority: ${this.priority}</li>`
-      )
+      //your code here
     }
   }
 
