178e9db689cbbf3ad7679fb7b25b2cf3a48918f2
Andrew Cohn <andrewcohn0@gmail.com>
2018-03-30 10:06:24 -0400

    add initial render to starter code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   // your solution here
+  // grab DOM elements
+  const listDiv = document.getElementById("app-content");
+
   const app = new TaskLister();
-  app.render();
+  const renderApp = () => listDiv.innerHTML = app.render();
+  renderApp();
 });
